e19d72ba5a55ec45e9119f36204aa42894be8a87
Chris Michels <michelsc@starktechgroup.com>
2025-02-03 11:55:34 -0500

    ff0 repl impl

--- src/main.zig
+++ src/main.zig
@@ -3,16 +3,16 @@ const std = @import("std");
 pub fn main() !void {
     // Uncomment this block to pass the first stage
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("$ ", .{});
-
     const stdin = std.io.getStdIn().reader();
+
     var buffer: [1024]u8 = undefined;
-    const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
+    while (true) {
+        try stdout.print("$ ", .{});
+        const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
 
-    // TODO: Handle user input
+        var it = std.mem.splitScalar(u8, user_input, ' ');
+        const command = it.next().?;
 
-    var it = std.mem.splitScalar(u8, user_input, ' ');
-    const command = it.next().?;
-
-    try stdout.print("{s}: command not found\n", .{command});
+        try stdout.print("{s}: command not found\n", .{command});
+    }
 }
